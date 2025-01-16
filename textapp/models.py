fe57a51f29383f212dce2af8d7fdf7490a54989d
laurence-lin <alksjdf12384@gmail.com>
2025-01-16 14:45:33 +0800

    Put data processing logic on seperate files, and add datetime column

--- textapp/models.py
+++ textapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 
 
 # Create your models here.
@@ -8,3 +8,4 @@ class TextInference(models.Model):
     inferenceText = models.TextField()
     task_type = models.CharField(max_length=100)
     user_ip = models.CharField(max_length=100, default="-1.-1.-1.-1")
+    update_time = models.DateTimeField(default=datetime.datetime.now())
