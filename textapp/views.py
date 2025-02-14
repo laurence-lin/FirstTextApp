ee5fc8ed7f606eb4109bc95735acde672194730e
laurence-lin <alksjdf12384@gmail.com>
2025-02-14 11:03:10 +0800

    Merge views.py from production to main on local

--- textapp/views.py
+++ textapp/views.py
@@ -28,6 +28,9 @@ def home(request):
     return render(request, 'home.html', home_data
     )
 
+def test(request):
+    return render(request, 'test.html')
+
 # Text Task webpage
 def base(request):
     return render(request, 'base.html')
@@ -50,13 +53,24 @@ def process_text(request):
         try:
             data = json.loads(request.body)
             text = data.get('text')
+
+            print("Full body: ", request.META)
+            # Get the client's IP address
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]  # Use the first IP in the list
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+
+            print(f"Client IP Address: {ip}")  # Log the IP address
+
             # Perform your text processing logic here (e.g., summarization)
             processed_text = text_processing(text) # Example: Convert to uppercase
 
             print("Data: ", data, ' text: ', text)
             print("Processed_test: ", processed_text)
 
-            format_data = {'originText':text, 'inferenceText':str(processed_text), 'task_type':'Normal'}
+            format_data = {'originText':text, 'inferenceText':str(processed_text), 'task_type':'Normal', 'user_ip': ip}
             print("Format data: ", format_data)
 
             # Store inference and source to DB
@@ -80,13 +94,20 @@ def nlp_text_summarize(request):
         try:
             data = json.loads(request.body)
             text = data.get('text')
+
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]  # Use the first IP in the list
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+
             # Perform your text processing logic here (e.g., summarization)
             processed_text = text_summarizing(text) # Example: Convert to uppercase
 
             print("Data: ", data, ' text: ', text)
             print("Processed_test: ", processed_text)
 
-            format_data = {'originText':text, 'inferenceText':str(processed_text), 'task_type':'Summarization'}
+            format_data = {'originText':text, 'inferenceText':str(processed_text), 'task_type':'Summarization', 'user_ip': ip}
             print("Format data: ", format_data)
 
             # Store inference and source to DB
@@ -119,3 +140,47 @@ class TextInferenceView(generics.ListCreateAPIView):
             return JsonResponse({'result': processed_text})
         except json.JSONDecodeError:
             return JsonResponse({'error': 'Invalid JSON'}, status=400)
+        
+
+# Text translate function
+@csrf_exempt # Important if you're not using Django's form submission
+def nlp_text_translate(request):
+    src_lang = 'eng_Latn'
+    target_lang = 'zho_Hans'
+    model = 'facebook/nllb-200-distilled-600M'
+
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            text = data.get('text')
+
+            x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+            if x_forwarded_for:
+                ip = x_forwarded_for.split(',')[0]  # Use the first IP in the list
+            else:
+                ip = request.META.get('REMOTE_ADDR')
+
+            # Perform your text processing logic here (e.g., summarization)
+            processed_text = text_translate(text,
+                                            checkpoint=model,
+                                            src_lang=src_lang, 
+                                            target_lang=target_lang) # Example: Convert to uppercase
+
+            print("Data: ", data, ' text: ', text)
+            print("Processed_test: ", processed_text)
+
+            format_data = {'originText':text, 'inferenceText':str(processed_text), 'task_type':'Translation', 'user_ip':ip}
+            print("Format data: ", format_data)
+
+            # Store inference and source to DB
+            serializer = TextInferenceSerializer(data=format_data)
+            if serializer.is_valid(raise_exception=True):
+                print("Input data valid! Deserialized to DB")
+                serializer.save()
+            else:
+                print("Input data serialization invalid!")
+
+            return JsonResponse({'result': processed_text})
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
